b67397f28809b945ebd536b8e70519490171defe
DevCulture <122065701+tuyulbodo99@users.noreply.github.com>
2026-05-21 23:52:50 +0700

    fix: ganti URL hokagelegend9999/original → hokagescript/main

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -547,7 +547,7 @@ sed -i '$ i}' /etc/nginx/conf.d/xray.conf
 
 sleep 1
 echo -e "[ ${green}INFO$NC ] Installing bbr.."
-wget -q -O /usr/bin/bbr "https://raw.githubusercontent.com/hokagelegend9999/original/main/ssh/bbr.sh"
+wget -q -O /usr/bin/bbr "https://raw.githubusercontent.com/tuyulbodo99/hokagescript/main/ssh/bbr.sh"
 chmod +x /usr/bin/bbr
 bbr >/dev/null 2>&1
 rm /usr/bin/bbr >/dev/null 2>&1
@@ -565,8 +565,8 @@ systemctl restart runn
 /etc/init.d/dropbear restart
 
 sleep 1
-wget -q -O /usr/bin/auto-set "https://raw.githubusercontent.com/hokagelegend9999/original/main/xray/auto-set.sh" && chmod +x /usr/bin/auto-set 
-wget -q -O /usr/bin/crtxray "https://raw.githubusercontent.com/hokagelegend9999/original/main/xray/crt.sh" && chmod +x /usr/bin/crtxray 
+wget -q -O /usr/bin/auto-set "https://raw.githubusercontent.com/tuyulbodo99/hokagescript/main/xray/auto-set.sh" && chmod +x /usr/bin/auto-set 
+wget -q -O /usr/bin/crtxray "https://raw.githubusercontent.com/tuyulbodo99/hokagescript/main/xray/crt.sh" && chmod +x /usr/bin/crtxray 
 sleep 1
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 yellow "xray/Vmess"
