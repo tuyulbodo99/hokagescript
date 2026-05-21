12646c4eee0a03a0764e9d69575724df8e65feeb
tuyulbodo99 <122065701+tuyulbodo99@users.noreply.github.com>
2026-05-21 08:45:31 +0700

    fix: sinkronkan ijin ke tuyulbodo99, hapus referensi hokage/devculturebot

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ biji=$(date +"%Y-%m-%d" -d "$dateFromServer")
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/devculturebot/ijin/main/youtube > /root/tmp
+    curl -sS https://raw.githubusercontent.com/tuyulbodo99/ijin/main/youtube > /root/tmp
     data=( $(cat /root/tmp | grep -E "^### " | awk '{print $2}') )
     for user in "${data[@]}"
     do
@@ -20,9 +20,9 @@ BURIQ () {
     done
     rm -f  /root/tmp
 }
-#https://raw.githubusercontent.com/devculturebot/ijin/main/original 
+#https://raw.githubusercontent.com/tuyulbodo99/ijin/main/original 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/devculturebot/ijin/main/youtube | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/tuyulbodo99/ijin/main/youtube | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/devculturebot/ijin/main/youtube | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/tuyulbodo99/ijin/main/youtube | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -192,7 +192,7 @@ fi
 fi
 
 echo ""
-wget -q https://raw.githubusercontent.com/devculturebot/devculturescript/main/dependencies.sh;chmod +x dependencies.sh;./dependencies.sh
+wget -q https://raw.githubusercontent.com/tuyulbodo99/devculture-vps/main/dependencies.sh;chmod +x dependencies.sh;./dependencies.sh
 rm dependencies.sh
 clear
 
@@ -244,20 +244,20 @@ EOF
 echo -e "$green[INFO]$NC Install SSH & OpenVPN!"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/devculturebot/devculturescript/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/tuyulbodo99/devculture-vps/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 #Instal Xray
 echo -e "$green[INFO]$NC Install Install XRAY!"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/devculturebot/devculturescript/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+wget https://raw.githubusercontent.com/tuyulbodo99/devculture-vps/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 clear
-wget https://raw.githubusercontent.com/devculturebot/original/main/backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/tuyulbodo99/hokagescript/main/backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 clear
-wget https://raw.githubusercontent.com/devculturebot/original/main/websocket/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+wget https://raw.githubusercontent.com/tuyulbodo99/hokagescript/main/websocket/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
 echo -e "$green[INFO]$NC Download Extra Menu"
 sleep 2
-wget https://raw.githubusercontent.com/devculturebot/original/main/update/update.sh && chmod +x update.sh && ./update.sh
+wget https://raw.githubusercontent.com/tuyulbodo99/hokagescript/main/update/update.sh && chmod +x update.sh && ./update.sh
 clear
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
@@ -284,7 +284,7 @@ if [ ! -f "/etc/log-create-user.log" ]; then
 echo "Log All Account " > /etc/log-create-user.log
 fi
 history -c
-serverV=$( curl -sS https://raw.githubusercontent.com/devculturebot/ipmini/main/versi  )
+serverV=$( curl -sS https://raw.githubusercontent.com/tuyulbodo99/hokagescript/main/versi  )
 echo $serverV > /opt/.ver
 aureb=$(cat /home/re_otm)
 b=11
